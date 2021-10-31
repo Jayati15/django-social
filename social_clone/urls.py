74cd5064b6aeb5d83fafd28c6f17c9d2e737f393
Jayati Dixit <77896511+Jayati15@users.noreply.github.com>
2021-10-31 16:52:57 +0530

    Update urls.py

--- social_clone/urls.py
+++ social_clone/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls import url
 from django.views.static import serve
+from django.conf import settings
 
 from .import views
 
@@ -30,7 +31,7 @@ urlpatterns = [
     path('posts/',include('posts.urls',namespace='posts')),
     path('groups/',include('groups.urls',namespace='groups')),
     
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     
     
