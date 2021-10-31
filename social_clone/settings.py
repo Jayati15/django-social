414a44a4cd10eca48c24dfea8e07f019e38a3a12
Jayati Dixit <77896511+Jayati15@users.noreply.github.com>
2021-10-31 18:12:26 +0530

    Update settings.py

--- social_clone/settings.py
+++ social_clone/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!0y1f0p^ja9g1q^_cm_&w)7(znb$u)**blkc8p^#o%3^eu*7l5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1:8000','sampledomain.com']
+ALLOWED_HOSTS = ['127.0.0.1:8000','sampledomain.com','jayati.pythonanywhere.com']
 
 
 # Application definition
